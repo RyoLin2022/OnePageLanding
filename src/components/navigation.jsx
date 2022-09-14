fd3c4116d7fad6d7fe3debdbb62dd033ca502d94
Ryo <samxyz258456@gmail.com>
2022-09-14 15:13:15 +0800

    ConnectWallet

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,4 +1,62 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { ethers } from 'ethers'
+import './navigation.css'
+let currentAccount = null;
 export const Navigation = (props) => {
+
+  const [walletAddress, setWalletAddress] = useState('')
+  async function changingAccount() {
+    if (window.ethereum) {
+      window.ethereum.on('accountsChanged', () => {
+        requestAccount()
+      })
+    }
+  }
+
+  useEffect(() => {
+    changingAccount()
+  }, [walletAddress])
+
+  async function requestAccount() {
+    console.log('Requesting account...')
+    try {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      })
+      setWalletAddress(accounts[0])
+      currentAccount = accounts[0]
+      sessionStorage.setItem('Account', currentAccount)
+      let lengthAcc = currentAccount.length
+      let PCButton = document.getElementById("connect-btnPC");
+      let MobileButton = document.getElementById("connect-btnMobile");
+
+      MobileButton.innerText =
+        currentAccount.substring(0, 4) + '...' + currentAccount.substring(lengthAcc - 4, lengthAcc)
+
+      PCButton.innerText =
+        currentAccount.substring(0, 4) + '...' + currentAccount.substring(lengthAcc - 4, lengthAcc)
+    } catch (error) {
+      console.log('error connecting')
+    }
+    //Check if Metamask Exist
+    if (window.ethereum) {
+      console.log('detected')
+    } else {
+      console.log('not detected')
+    }
+  }
+
+  async function connectWallet() {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      alert('Wallet connected successfully!')
+    } else {
+      alert('Please install an injected Web3 wallet')
+    }
+  }
+
   return (
     <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
       <div className='container'>
@@ -18,6 +76,9 @@ export const Navigation = (props) => {
           <a className='navbar-brand page-scroll' href='#page-top'>
             Space ETH
           </a>{' '}
+          <button onClick={connectWallet} id="connect-btnMobile" className="ForMobile">
+            Connect Wallet
+          </button>
         </div>
 
         <div
@@ -49,6 +110,11 @@ export const Navigation = (props) => {
               <a href='#contact' className='page-scroll'>
                 Contact
               </a>
+            </li>
+            <li>
+              <button onClick={connectWallet} id="connect-btnPC" className="ForPC">
+                Connect Wallet
+              </button>
             </li>
           </ul>
         </div>
